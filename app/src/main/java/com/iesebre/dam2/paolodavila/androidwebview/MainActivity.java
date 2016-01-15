ed19387efe7ff3bff948a242de6e1604775113f5
Paolo Davila <paolodavila@iesebre.com>
2016-01-15 16:26:30 +0100

    Second version: Enabling javascript

--- app/src/main/java/com/iesebre/dam2/paolodavila/androidwebview/MainActivity.java
+++ app/src/main/java/com/iesebre/dam2/paolodavila/androidwebview/MainActivity.java
@@ -2,6 +2,7 @@ package com.iesebre.dam2.paolodavila.androidwebview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
         this.setContentView(R.layout.activity_main);
 
         WebView myWebView = (WebView) this.findViewById(R.id.webView);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
         myWebView.loadUrl("http://paolodavila.com");
     }
 }
